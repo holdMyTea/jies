361acba9f65c0308e564fda287b1822cd76d8d80
rise42 <rise42@yahoo.com>
2017-03-28 16:34:54 +0300

    config refactor

--- config/variables.js
+++ config/variables.js
@@ -1,25 +1,16 @@
 'use strict';
 
-let vars = {};
+let vars = {
+	APP_PORT: process.env.APP_PORT,
+	DB_HOST: process.env.DB_HOST,
+	DB_USER: process.env.DB_USER,
+	DB_PASS: process.env.DB_PASS,
+	DB_DATABASE: process.env.DB_DATABASE
+};
 
-if(process.env.APP_PORT != undefined)
-	vars.APP_PORT = process.env.APP_PORT;
-else throw new Error('Enviroment variable is missing');
-
-if(process.env.DB_HOST != undefined)
-	vars.DB_HOST = process.env.DB_HOST;
-else throw new Error('Enviroment variable is missing');
-
-if(process.env.DB_USER != undefined)
-	vars.DB_USER = process.env.DB_USER;
-else throw new Error('Enviroment variable is missing');
-
-if(process.env.DB_PASS != undefined)
-	vars.DB_PASS = process.env.DB_PASS;
-else throw new Error('Enviroment variable is missing');
-
-if(process.env.DB_DATABASE != undefined)
-	vars.DB_DATABASE = process.env.DB_DATABASE;
-else throw new Error('Enviroment variable is missing');
+for(const key in vars){
+	if(!vars[key])
+		throw new Error('Enviromental property '+key+' is missing');
+}
 
 export default vars;
