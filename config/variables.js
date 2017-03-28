41f113c91ebb6ed13e538da3875d3211403c90ee
rise42 <rise42@yahoo.com>
2017-03-28 22:55:12 +0300

    Fancy config

--- config/variables.js
+++ config/variables.js
@@ -1,16 +1,11 @@
 'use strict';
 
-let vars = {
-	APP_PORT: process.env.APP_PORT,
-	DB_HOST: process.env.DB_HOST,
-	DB_USER: process.env.DB_USER,
-	DB_PASS: process.env.DB_PASS,
-	DB_DATABASE: process.env.DB_DATABASE
-};
-
-for(const key in vars){
-	if(!vars[key])
-		throw new Error('Enviromental property '+key+' is missing');
-}
-
-export default vars;
+export default ['APP_PORT','DB_HOST','DB_USER','DB_PASS','DB_DATABASE']
+.reduce((acc, val) => {
+	if(process.env[val]){
+		acc[val] = process.env[val];
+		return acc;
+	}
+	else
+		throw new Error('Enviromental property '+val+' is missing');
+}, {});
